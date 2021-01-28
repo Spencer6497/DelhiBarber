dbd5d97b7df1c65dd05c1fa38bc2a63c6fd70d51
Spencer Peace <peaces2@nku.edu>
2021-01-28 10:25:47 -0500

    change php script to use PHPMailer

--- php/script.php
+++ php/script.php
@@ -1,58 +1,82 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer(true);
   
-if($_POST) {
-    $name = "";
-    $email = "";
-    $subject = "";
-    $comment = "";
-    $email_body = "<div>";
-      
-    if(isset($_POST['name'])) {
-        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
-                        </div>";
-    }
- 
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['subject'])) {
-        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$subject."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['comment'])) {
-        $comment = htmlspecialchars($_POST['comment']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$comment."</div>
-                        </div>";
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.google.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'supergamer64973@gmail.com';                     // SMTP username
+    $mail->Password   = 'Usaf6497';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    if($_POST) {
+        $name = "";
+        $email = "";
+        $subject = "";
+        $comment = "";
+        $email_body = "<div>";
+          
+        if(isset($_POST['name'])) {
+            $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+            $email_body .= "<div>
+                               <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
+                            </div>";
+        }
+     
+        if(isset($_POST['email'])) {
+            $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+            $email_body .= "<div>
+                               <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                            </div>";
+        }
+          
+        if(isset($_POST['subject'])) {
+            $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+            $email_body .= "<div>
+                               <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$subject."</span>
+                            </div>";
+        }
+          
+        if(isset($_POST['comment'])) {
+            $comment = htmlspecialchars($_POST['comment']);
+            $email_body .= "<div>
+                               <label><b>Visitor Message:</b></label>
+                               <div>".$comment."</div>
+                            </div>";
+        }
+    
+        $recipient = "spencer.peace6497@yahoo.com";
+          
+        $email_body .= "</div>";
+     
+        $headers  = 'MIME-Version: 1.0' . "\r\n"
+        .'Content-type: text/html; charset=utf-8' . "\r\n"
+        .'From: ' . $email . "\r\n";
     }
 
-    $recipient = "spencer.peace6497@yahoo.com";
-      
-    $email_body .= "</div>";
- 
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $email . "\r\n";
-      
-    if(mail($recipient, $subject, $email_body, $headers)) {
-        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo "<p>Name: $name, Subject: $subject, Body: $email_body, Headers: $headers </p>";
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-      
-} else {
-    echo '<p>Something went wrong</p>';
+    //Recipients
+    $mail->setFrom($email);
+    $mail->addAddress($email);
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $email_body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
